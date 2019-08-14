3ba5e9a4282d50def21cd0400673d31c025d09d6
79527803328 <michail89527803328@outlook.com>
2019-08-14 15:36:16 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace MM
+{
+    partial class Math_Malceva
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Math_Malceva));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textEps = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.N = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.An = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Bn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.T = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(164, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textA
+            // 
+            this.textA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textA.Location = new System.Drawing.Point(58, 9);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 31);
+            this.textA.TabIndex = 2;
+            // 
+            // textB
+            // 
+            this.textB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textB.Location = new System.Drawing.Point(58, 46);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 31);
+            this.textB.TabIndex = 3;
+            // 
+            // textEps
+            // 
+            this.textEps.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textEps.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textEps.Location = new System.Drawing.Point(58, 83);
+            this.textEps.Name = "textEps";
+            this.textEps.Size = new System.Drawing.Size(100, 31);
+            this.textEps.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(164, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            this.N,
+            this.An,
+            this.Bn,
+            this.T});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.Location = new System.Drawing.Point(12, 130);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(864, 338);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Width = 0;
+            // 
+            // N
+            // 
+            this.N.Text = "N";
+            this.N.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.N.Width = 65;
+            // 
+            // An
+            // 
+            this.An.Text = "An";
+            this.An.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.An.Width = 265;
+            // 
+            // Bn
+            // 
+            this.Bn.Text = "Bn";
+            this.Bn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Bn.Width = 265;
+            // 
+            // T
+            // 
+            this.T.Text = "T";
+            this.T.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.T.Width = 265;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 31);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "A:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 31);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "B:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "E:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(164, 83);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 31);
+            this.progressBar1.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(270, 46);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 31);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "чистить A,B,N";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(814, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 31);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Exit";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Window;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(383, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(425, 106);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Значение A - начало отрезка\r\nЗначение B - конец отрезка\r\nЗначение E - епсилон, то" +
+    "чность";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox2.Location = new System.Drawing.Point(270, 83);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(110, 31);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "убирать последнее";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Math_Malceva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.DarkOrange;
+            this.ClientSize = new System.Drawing.Size(888, 480);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textEps);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Math_Malceva";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Math_Malceva";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textEps;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader N;
+        private System.Windows.Forms.ColumnHeader An;
+        private System.Windows.Forms.ColumnHeader Bn;
+        private System.Windows.Forms.ColumnHeader T;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace MM
+{
+    public partial class Math_Malceva : Form
+    {
+        public Math_Malceva()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                progressBar1.Value = 0;
+
+                textA.Enabled = false;
+                textB.Enabled = false;
+                textEps.Enabled = false;
+
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+
+                checkBox2.Enabled = false;
+
+                double a = 0, b = 0;
+                double c = 0, f = 0;
+                double eps = 0, t = 1;
+                int j = 0;
+
+                a = Convert.ToDouble(textA.Text);
+                b = Convert.ToDouble(textB.Text);
+                eps = Convert.ToDouble(textEps.Text);
+
+                while (eps <= t)
+                {
+                    progressBar1.Value++;
+                    Thread.Sleep(50);
+
+                    c = (a + b) / 2;
+                    f = 5 * Math.Sin(c) - c;
+                    t = (b - a) / 2;
+                    j++;
+
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.SubItems.Add(Convert.ToString(j)); //первый
+                    lvi.SubItems.Add(Convert.ToString(a)); //второй
+                    lvi.SubItems.Add(Convert.ToString(b)); //третий
+                    lvi.SubItems.Add(Convert.ToString(t)); //четвертый
+                    listView1.Items.Add(lvi);
+
+                    if (f > 0)
+                    {
+                        a = c;
+                    }
+                    else
+                    {
+                        b = c;
+                    }
+                }
+
+                if(checkBox2.Checked == true)
+                {
+                    listView1.Items.RemoveAt(j-1);
+                }          
+
+                progressBar1.Value = 100;
+
+                textA.Enabled = true;
+                textB.Enabled = true;
+                textEps.Enabled = true;
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                checkBox2.Enabled = true;
+            }
+            catch (FormatException)
+            {
+                textA.Enabled = true;
+                textB.Enabled = true;
+                textEps.Enabled = true;
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                checkBox2.Enabled = true;
+
+                progressBar1.Value = 0;
+
+                MessageBox.Show("Сработала защита - FormatException, неправильно введены значения!");
+            }
+            catch (OverflowException)
+            {
+                textA.Enabled = true;
+                textB.Enabled = true;
+                textEps.Enabled = true;
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                checkBox2.Enabled = true;
+
+                progressBar1.Value = 0;
+
+                MessageBox.Show("Сработала защита - OverflowException, переполнение!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+
+            if(checkBox1.Checked == true)
+            {
+                textA.Text = "";
+                textB.Text = "";
+                textEps.Text = "";
+            }
+
+            progressBar1.Value = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
